4ed8a7d0f88cb6da750d8c35ac85548f521a8ad3
mojo <igormalj@gmail.com>
2024-12-30 15:56:22 +0100

    add api protection

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import "dotenv/config";
 import express from "express";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
+import cors from "cors";
 //
 import routes from "./routes/index.js";
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 50,
+});
+
 const port = process.env.PORT;
 const app = express();
-
+//
+app.use(helmet());
+app.use(limiter);
+app.use(cors());
 // body parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
