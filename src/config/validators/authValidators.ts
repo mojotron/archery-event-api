3dee60e2dcbaf67b637046e863fad0760171ed29
mojo <igormalj@gmail.com>
2025-01-04 17:05:19 +0100

    add more validation

--- src/config/validators/authValidators.ts
+++ src/config/validators/authValidators.ts
@@ -10,16 +10,43 @@ const emailValidation = body("email")
   .withMessage(`email is invalid`)
   .normalizeEmail()
   .escape();
-const passwordValidation = body("password");
+
+const passwordValidation = body("password")
+  .trim()
+  .notEmpty()
+  .withMessage("password must not be empty")
+  .matches(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,}$/)
+  .withMessage(
+    `password must be be minimum of 8 character and including uppercase letter, lowercase letter, number and special character @$!%*?&`
+  )
+  .escape();
 
 const signupValidator = [
-  body("username"),
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage(`username field is empty`)
+    .isString()
+    .withMessage(`username field must be string of characters`)
+    .isLength({ min: 3, max: 10 })
+    .withMessage(`username length must be between`)
+    .matches(/^$/)
+    .withMessage(`username accepts`)
+    .escape(),
 
   emailValidation,
 
   passwordValidation,
 
-  body("confirmPassword"),
+  check("confirmPassword")
+    .trim()
+    .custom((confirmPassword, { req }) => {
+      const password = req.body.password;
+      if (password !== confirmPassword) return false;
+      return true;
+    })
+    .withMessage(`password and confirm password not matching`)
+    .escape(),
 ];
 
 const loginValidator = [];
