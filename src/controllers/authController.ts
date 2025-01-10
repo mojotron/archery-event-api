c66737eb83908926a24eb89b267a73b279fbe026
mojo <igormalj@gmail.com>
2025-01-10 20:40:28 +0100

    edit controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-const signup = async () => {
+const signup = async (req: Request, res: Response, next: NextFunction) => {
   try {
   } catch (error) {}
 };
