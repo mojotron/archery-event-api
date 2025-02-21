a85b52ced314fa7a7cac2974d9381ebe0762b24e
mojo <igormalj@gmail.com>
2025-02-21 06:55:15 +0100

    add structure in season logic

--- src/controllers/season.controller.ts
+++ src/controllers/season.controller.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
 import catchErrors from "../utils/catchErrors.js";
+import { createSeasonSchema } from "./season.schema.js";
+import { OK } from "../constants/http.js";
+import { createSeason } from "../services/season.service.js";
 
 export const createSeasonHandler = catchErrors(
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const request = createSeasonSchema.parse({ ...req.body });
+
+    await createSeason();
+
+    return res.status(OK);
+  }
 );
--- src/controllers/season.schema.ts
+++ src/controllers/season.schema.ts
@@ -1,7 +1,11 @@
+import { SeasonType } from "@prisma/client";
 import { z } from "zod";
+
+const seasonTypeX = typeof SeasonType;
 
 export const createSeasonSchema = z.object({
   title: z.string().trim(),
   description: z.string().trim(),
   tournamentCount: z.number(),
+  //seasonType: z.enum(seasonTypeX),
 });
