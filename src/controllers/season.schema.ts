31fca7f45e07a41fbe2115271c07fafaadf9af0f
mojo <igormalj@gmail.com>
2025-02-22 20:34:14 +0100

    add minor fix

--- src/controllers/season.schema.ts
+++ src/controllers/season.schema.ts
@@ -1,11 +1,9 @@
 import { SeasonType } from "@prisma/client";
 import { z } from "zod";
 
-const seasonTypeX = typeof SeasonType;
-
 export const createSeasonSchema = z.object({
   title: z.string().trim(),
   description: z.string().trim(),
   tournamentCount: z.number(),
-  //seasonType: z.enum(seasonTypeX),
+  seasonType: z.nativeEnum(SeasonType),
 });
--- src/services/season.service.ts
+++ src/services/season.service.ts
@@ -1,1 +1,12 @@
-export const createSeason = async () => {};
+import { SeasonType } from "@prisma/client";
+
+type CreateSeasonParma = {
+  title: string;
+  description: string;
+  type: SeasonType;
+  tournamentCount: number;
+};
+
+export const createSeason = async (data: CreateSeasonParma) => {
+  //
+};
